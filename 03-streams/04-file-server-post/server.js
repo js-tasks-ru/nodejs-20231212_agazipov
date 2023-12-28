0b05ce153d0ab47bda7ad27f131245a5b6edb6cb
Alexander Gorbunov <agazipov@gmail.com>
2023-12-28 16:40:11 +0500

    todo hm-3

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -8,19 +8,17 @@ const server = new http.Server();
 
 
 server.on('request', (req, res) => {
-  console.log('__size__', req.headers['content-length']);
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
   const filepath = path.join(__dirname, 'files', pathname);
   const streamLimit = new LimitSizeStream({
-    limit: 1000000,
+    limit: 4300000,
     readableObjectMode: false,
   });
 
   switch (req.method) {
     case 'POST':
-      console.log('________________________________');
 
       if (pathname.includes('/')) {
         res.statusCode = 400;
@@ -29,78 +27,43 @@ server.on('request', (req, res) => {
       }
 
       if (fs.existsSync(filepath)) {
-        console.log('файл есть');
         res.statusCode = 409;
         res.end('change name');
         break;
       }
 
-      // if (parseInt(req.headers['content-length']) > 999999) {
-      //   res.statusCode = 413;
-      //   res.end('max size');
-      //   break;
-      // }
-
       const writable = fs.createWriteStream(filepath);
       req.pipe(streamLimit).pipe(writable);
-
-      // дубль файла ** код ошибки
-      writable.on('error', (err) => {
-        console.error('readERR', err);
-        res.statusCode = 409;
-        res.end('change name');
-      });
-      // большой размер
-      streamLimit.on('error', (err) => {
-        console.error(`streamLimitERR`, err.message);
-        writable.end();
-        writable.on('close', (e) => {
-          fs.unlink(filepath, (err2) => {
-            if (err2) throw err2;
-            console.log('path/file.txt was deleted');
-          });
-        });
-        res.statusCode = 413;
-        res.end('max size');
-      });
-      // дисконект
-      req.on('error', (err) => {
-        console.error(err.message);
-        if (err.code === 'ECONNRESET' || err.code === 'EPIPE' || err.code === 'ECONNABORTED') {
-          streamLimit.end();
-          writable.end();
-          fs.unlink(filepath, (err2) => {
-            if (err2) throw err2;
-            console.log('path/file.txt was deleted obryv');
-          });
-          res.statusCode = 404;
-          res.end('no entry files');
-        } else {
-          res.statusCode = 500;
-          res.end('other error');
-        }
-      });
 
       writable.on('finish', () => {
         res.statusCode = 201;
         res.end('succes');
       });
 
-      req.on('close', () => {
-        console.log(`req close`);
+      // большой размер
+      streamLimit.on('error', (err) => {
+        console.error(`streamLimitERR`, err.message);
+        writable.end();
+        fs.unlinkSync(filepath, (err) => {
+          if (err) throw err;
+        });
+        res.statusCode = 413;
+        res.end('max size');
       });
-      req.on('end', () => {
-        console.log('reqEnd');
-      });
-      streamLimit.on('close', () => {
-        console.log('streamLimit close');
-      });
-      streamLimit.on('pipe', () => {
-        console.log('streamLimit pipe');
-      });
-
-      writable.on('close', () => {
-        console.log('wrible close');
+      // дисконект
+      req.on('error', (err) => {
+        if (err.code === 'ECONNRESET' || err.code === 'EPIPE' || err.code === 'ECONNABORTED') {
+          streamLimit.end();
+          writable.end();
+          fs.unlink(filepath, (err2) => {
+            if (err2) throw err2;
+          });
+          res.statusCode = 600;
+          res.end('no entry files');
+          return;
+        }
+        res.statusCode = 500;
+        res.end('other error');
       });
 
       break;
