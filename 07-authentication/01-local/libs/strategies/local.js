664454107c2ba9406ffebd1079a84a322ff8b7c6
Alexander Gorbunov <agazipov@gmail.com>
2024-01-18 17:19:30 +0500

    07/01 hw complete

--- 07-authentication/01-local/libs/strategies/local.js
+++ 07-authentication/01-local/libs/strategies/local.js
@@ -1,8 +1,16 @@
+const User = require('../../models/User');
+
 const LocalStrategy = require('passport-local').Strategy;
 
 module.exports = new LocalStrategy(
     {usernameField: 'email', session: false},
     function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
+      User.findOne({email: email}, async function(err, user) {
+        if (err) done(err);
+        if (!user) return done(null, false, 'Нет такого пользователя');
+        const check = await user.checkPassword(password);
+        if (!check) return done(null, false, 'Неверный пароль');
+        return done(null, user, 'Здарова отец');
+      });
     },
 );
