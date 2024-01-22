a7818c8eb19aecdbb636146d3bf696176ceddea3
Alexander Gorbunov <agazipov@gmail.com>
2024-01-22 23:06:21 +0500

    2task

--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,15 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  if (!email) return done(null, false, 'Не указан email');
+  try {
+    const user = await User.findOne({email: email});
+    if (!user) {
+      const newUser = await User.create({email, displayName});
+      return done(null, newUser);
+    }
+    done(null, user, 'нашел тебя');
+  } catch (error) {
+    done(error);
+  }
 };
