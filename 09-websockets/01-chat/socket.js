ffe4ee16918bb8dca02c24581f25a7e530fb2d71
Alexander Gorbunov <agazipov@gmail.com>
2024-01-29 20:05:02 +0500

    create 9hw

--- 09-websockets/01-chat/socket.js
+++ 09-websockets/01-chat/socket.js
@@ -7,11 +7,33 @@ function socket(server) {
   const io = socketIO(server);
 
   io.use(async function(socket, next) {
+    console.log('token', socket.handshake.query.token);
+    if (!socket.handshake.query.token) {
+      return next(new Error('anonymous sessions are not allowed'));
+    }
+    const session = await Session.findOne({token: socket.handshake.query.token}).populate('user');
+    if (!session) {
+      throw new Error('wrong or expired session token');
+    }
+    socket.user = session.user;
     next();
   });
 
   io.on('connection', function(socket) {
-    socket.on('message', async (msg) => {});
+    const {displayName, _id} = socket.user;
+    socket.on('message', async (msg) => {
+      await Message.create({
+        user: displayName,
+        chat: _id,
+        text: msg,
+        date: new Date(),
+      });
+      socket.broadcast.emit(msg);
+    });
+  });
+
+  io.on('connect_error', (err) => {
+    console.log(err.message);
   });
 
   return io;
